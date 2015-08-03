31d55d80f1aef3ba18c086b458b33e58f77de02d
wuxingyu1983 <wuxingyu@wuxingyudeMacBook-Pro.local>
2015-08-03 23:13:44 +0800

    去掉老的代码

--- database.js
+++ database.js
@@ -1,23 +1,7 @@
 /**
  * Created by wuxingyu on 15/7/28.
  */
-/*
-function saveAlbum(name, url, db) {
-    var collection = db.collection('album');
 
-    collection.findOne({name: name, url: url}, {limit: 1}, function (err, result) {
-        if (null == result) {
-            collection.insertOne({name: name, url: url}, null, function (err, result) {
-                if (err) {
-                    console.log("insert data error!!");
-                }
-            });
-        }
-        else {
-        }
-    });
-}
-*/
 function saveAlbum(albums, db, callback) {
     if (0 < albums.length) {
         var collection = db.collection('album');
